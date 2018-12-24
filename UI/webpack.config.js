81ef6764902c98d51a7331f0cd33b9c7f553799b
Nag-Training <nag-training@NAGABHUSHANAMs-MacBook-Pro.local>
2018-12-24 10:59:26 +0530

    webpack config

---
+++ UI/webpack.config.js
@@ -0,0 +1,19 @@
+
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    mode: 'development',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })],
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        disableHostCheck: true,
+        port: 3000
+    }
+};
