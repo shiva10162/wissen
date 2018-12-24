1a047dfc36c25ec670ec81437f406de750434f96
Nag-Training <nag-training@NAGABHUSHANAMs-MacBook-Pro.local>
2018-12-24 11:16:47 +0530

    sass config

--- UI/src/index.js
+++ UI/src/index.js
@@ -1,1 +1,3 @@
 console.log("-index.js-");  
+
+import './index.scss'
--- UI/webpack.config.js
+++ UI/webpack.config.js
@@ -9,6 +9,22 @@ module.exports = {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
     },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    "style-loader", // creates style nodes from JS strings
+                    "css-loader", // translates CSS into CommonJS
+                    "sass-loader" // compiles Sass to CSS, using Node Sass by default
+                ]
+            }
+        ],
+    },
     plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })],
     devServer: {
         contentBase: path.join(__dirname, 'dist'),
